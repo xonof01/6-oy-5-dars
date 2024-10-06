551d83da60e8724daf151f72d17f8484083930c6
firdavsxon@gmail.com <firdavsxon@gmail.com>
2024-10-07 01:43:57 +0500

    PUSH

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Home from '"./pages/Home"
+import About from "./pages/About'
+import profile from "./pages/Profile'
+import Contact from " ./pages/Contact
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header/>
+    <Routes>
+      <Route phate>
+      <Route>
+      </Route>
+      </Route>
+    </Routes>
+    </>
+  )
+}
+
+export default App
